c96e0ef5e9dd2e3beb6db4f01292d8fbf5fa6dad
brew4321 <brew4321@gmail.com>
2016-07-01 15:10:16 -0600

    test a master list

---
+++ masterlist.js
@@ -0,0 +1,3 @@
+var owned = [
+    {group: "origin", owner: "Nobody", projectURL: "https://www.khanacademy.org/computer-programming/isometric-design-collaboration/5409133691273216", x:0,y:0,w:50,h:50},
+];
