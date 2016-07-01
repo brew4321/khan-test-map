f3dd0ec8cc56e2c4fc670f2637080c74b9394133
brew4321 <brew4321@gmail.com>
2016-07-01 15:30:53 -0600

    put level array directly in master list object

--- masterlist.js
+++ masterlist.js
@@ -1,3 +1,13 @@
 var owned = [
-    {group: "origin", owner: "Nobody", projectURL: "https://www.khanacademy.org/computer-programming/isometric-design-collaboration/5409133691273216", x:0,y:0,w:50,h:50},
+    {group: "origin", owner: "Nobody", projectURL: "https://www.khanacademy.org/computer-programming/isometric-design-collaboration/5409133691273216", x:0,y:0,w:50,h:50,level: [
+	"  gttggmg   ",
+	" gmmggggt  ",
+	"gwggwwwttt",
+	"ttwghgwwgt",
+	"gtwmpgwgtt",
+	" gwgpgwwgg",
+	" gwgpgwggg",
+	"  ghpgghgg",
+	"    ",
+]},
 ];
